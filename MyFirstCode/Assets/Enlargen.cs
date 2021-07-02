26b2c702e1ee1bf9d8615d2e7e02135b4a1a2e12
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2021-07-02 21:38:48 +0200

    31. Our first Code

---
+++ MyFirstCode/Assets/Enlargen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enlargen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale *= 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
