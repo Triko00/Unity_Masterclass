2e705b765ae4dc5b94fb42188b40fe5d98a25845
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2021-08-25 14:52:41 +0200

    43. Working with Text Objects in Unity and Code

--- GuessTheNumber/Assets/Scripts/GameLogic.cs
+++ GuessTheNumber/Assets/Scripts/GameLogic.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 public class GameLogic : MonoBehaviour
 {
     public InputField userInput;
+    public Text gameLabel;
+
+
     public int randomNum;
 
     // Start is called before the first frame update
@@ -17,13 +20,34 @@ public class GameLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     public void OnButtonClick()
     {
-        Debug.Log(userInput.text);
-        Debug.Log("the totally random number is: " + randomNum + ".");
+        string userInputValue = userInput.text;
+        if(userInputValue != "")
+        {
+            int answer = int.Parse(userInputValue);
+
+            if (answer == randomNum)
+            {
+                gameLabel.text = "wow";
+            }
+            else if (answer < randomNum)
+            {
+                gameLabel.text = "higha";
+            }
+            else if (answer > randomNum)
+            {
+                gameLabel.text = "lowa";
+            }
+        }
+
+        else
+        {
+            gameLabel.text = "uuuh, im not a mind reader.";
+        }
     }
 
 }
