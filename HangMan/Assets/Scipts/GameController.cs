bcce19f8d04b5582bac4dede718cf8db54c685ba
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2021-08-28 13:49:01 +0200

    63. Winning or Losing the game

--- HangMan/Assets/Scipts/GameController.cs
+++ HangMan/Assets/Scipts/GameController.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public Text timeField;
+    public GameObject[] hangMan;
+    public GameObject winText;
+    public GameObject loseText;
     public Text wordToFindField;
     private float time;
     private string[] wordsLocal = { "TERRARIA", "MINECRAFT", "APEX LEGENDS", "FORTNITE", "GENSHIN IMPACT", "FARCRY", "KARLSON", "WARCRAFT", "SPELLBREAK", "TITANFALL", "CALL OF DUTY" };
     private string chosenWord;
     private string hiddenWord;
+    private int fails;
+    private bool gameEnd = false;
 
 
     // Start is called before the first frame update
@@ -32,7 +37,7 @@ public class GameController : MonoBehaviour
             }
             else
             {
-                hiddenWord += "_ ";
+                hiddenWord += "-";
             }
 
             
@@ -45,7 +50,55 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        timeField.text = time.ToString();
+        if (gameEnd == false)
+        {
+            time += Time.deltaTime;
+            timeField.text = time.ToString();
+        }
     }
+
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if(e.type == EventType.KeyDown && e.keyCode.ToString().Length == 1)
+        {
+            string pressedLetter = e.keyCode.ToString();
+
+            Debug.Log("KeyDown event has been triggered " + pressedLetter);
+
+            if(chosenWord.Contains(pressedLetter))
+            {
+                int i = chosenWord.IndexOf(pressedLetter);
+                while(i != -1)
+                {
+                    hiddenWord = hiddenWord.Substring(0, i) + pressedLetter + hiddenWord.Substring(i + 1);
+
+                    chosenWord = chosenWord.Substring(0, i) + "-" + chosenWord.Substring(i + 1);
+
+
+                    i =  chosenWord.IndexOf(pressedLetter);
+                }
+
+                wordToFindField.text = hiddenWord;
+
+            }
+            else
+            {
+                hangMan[fails].SetActive(true);
+                fails++;
+            }
+            if(fails == hangMan.Length)
+            {
+                loseText.SetActive(true);
+                gameEnd = true;
+            }
+            if(!hiddenWord.Contains("-"))
+            {
+                winText.SetActive(true);
+                gameEnd = true;
+            }
+        }
+    }
+
 }
